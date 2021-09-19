4fea87532bfcd06557dffa4c2c11194f82748890
mryoomac <88038516+mryoomac@users.noreply.github.com>
2021-09-19 19:17:48 +0200

    fix
    add ProductModule to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,13 +2,14 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductModule } from './product/product.module';
 import { UserModule } from './user/user.module';
 
 
 
 @Module({
   
-  imports: [MongooseModule.forRoot('mongodb+srv://MongoClient:haslobezcyfrjestdobre@cluster0.g9ws1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'), UserModule],
+  imports: [MongooseModule.forRoot('mongodb+srv://MongoClient:haslobezcyfrjestdobre@cluster0.g9ws1.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'), UserModule, ProductModule],
   controllers: [AppController],
   providers: [AppService],
 })
